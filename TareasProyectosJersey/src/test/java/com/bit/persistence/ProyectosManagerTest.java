3fbb98245cc880558baea42d9fb8d4107ec5a67f
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-27 12:32:02 +0200

    update to banana

---
+++ TareasProyectosJersey/src/test/java/com/bit/persistence/ProyectosManagerTest.java
@@ -0,0 +1,37 @@
+package com.banana.persistence;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.banana.models.Proyecto;
+import com.banana.models.Tarea;
+
+public class ProyectosManagerTest {
+
+	private static SessionFactory sf = null;
+
+	//@BeforeClass
+	public static void createSessionFactory() {
+		sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	}
+
+	//@Test
+	public void testGetProyectos() {
+		try {
+			List<Proyecto> listT= ProyectosManager.getInstance().getProyectos();
+			assertNotNull(listT);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+}
